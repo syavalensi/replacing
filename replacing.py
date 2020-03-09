7a49f9863be10a9a0120d04b6ad90f653175a548
syavalensi <61826638+syavalensi@users.noreply.github.com>
2020-03-09 14:38:01 +0800

    Add files via upload

---
+++ replacing.py
@@ -0,0 +1,23 @@
+from PIL import Image 
+
+def main(): 
+	try: 
+		#Relative Path 
+		#Image on which we want to paste 
+		img = Image.open("robot.jpg") 
+		
+		#Relative Path 
+		#Image which we want to paste 
+		img2 = Image.open("picture3.jpg") 
+		img.paste(img2, (50, 50)) 
+		
+		#Saved in the same relative location 
+		img.save("pasted_picture.jpg") 
+		
+	except IOError: 
+		pass
+
+if __name__ == "__main__": 
+	main() 
+
+##An additional argument for an optional image mask image is also available. 
